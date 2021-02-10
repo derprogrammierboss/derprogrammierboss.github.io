0584ea07b9ec17a92904f61fcfa7143487a8a0dc
levinstaudt <58470581+derprogrammierboss@users.noreply.github.com>
2021-02-10 21:23:28 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function revealMessage() {
-  var x = document.getElementById("hiddenMessage");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
